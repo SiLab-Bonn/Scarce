b69088a50f06df7f2e156b097f976e6a95c31a7f
DavidLP <david-leon.pohl@rub.de>
2016-10-26 19:45:44 +0200

    ENH

--- siliconproperties/python_files/testFEM.py
+++ siliconproperties/python_files/testFEM.py
@@ -3,23 +3,42 @@
 '''
 
 import pygmsh as pg
+import numpy as np
 
 
 def generate(resolution):
     geom = pg.Geometry()
 
-    circle = geom.add_circle(x0=[0.0, 0.0, 0.0],
-                             radius=1.0,
-                             lcar=resolution,
-                             num_sections=4,
-                             # If compound==False, the section borders have to be points of the
-                             # discretization. If using a compound circle, they don't; gmsh can
-                             # choose by itself where to point the circle points.
-                             compound=True
-                             )
+    # Two readout pillars
+    circle_left = geom.add_circle(x0=[-1.5, 0.0, 0.0],
+                                  radius=0.5,
+                                  lcar=resolution / 2.,
+                                  num_sections=4,
+                                  # If compound==False, the section borders have to be points of the
+                                  # discretization. If using a compound circle, they don't; gmsh can
+                                  # choose by itself where to point the circle points.
+                                  compound=False
+                                  )
+    circle_left_ll = geom.add_line_loop(circle_left)
 
-    ll = geom.add_line_loop(circle)
-    geom.add_plane_surface(ll)
+    circle_right = geom.add_circle(x0=[1.5, 0.0, 0.0],
+                                   radius=0.5,
+                                   lcar=resolution / 2.,
+                                   num_sections=4,
+                                   # If compound==False, the section borders have to be points of the
+                                   # discretization. If using a compound circle, they don't; gmsh can
+                                   # choose by itself where to point the circle points.
+                                   compound=False
+                                   )
+    circle_right_ll = geom.add_line_loop(circle_right)
+
+    geom.add_rectangle(xmin=-3,
+                       xmax=3,
+                       ymin=-3,
+                       ymax=3,
+                       z=0,
+                       lcar=resolution,
+                       holes=[circle_left_ll, circle_right_ll])
 
     return geom
 
@@ -30,8 +49,9 @@ if __name__ == '__main__':
 
     # Plot triangle cells
     plt.plot(points[cells['triangle'].T, 0], points[cells['triangle'].T, 1], '-', color='black')
-    plt.xlim((-1.1, 1.1))
-    plt.ylim((-1.1, 1.1))
+    plt.plot(points[cells['line'].T, 0], points[cells['line'].T, 1], '-', color='black')
+#     plt.xlim((-2.1, 2.1))
+#     plt.ylim((-2.1, 2.1))
     plt.show()
 
     # Save mesh
