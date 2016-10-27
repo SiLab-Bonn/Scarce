b4e82b6f568ec363cf4a93ddb2487ae2f0c33496
DavidLP <David-Leon.Pohl@rub.de>
2016-10-27 23:15:46 +0200

    MAINT

--- siliconproperties/python_files/testFEM.py
+++ siliconproperties/python_files/testFEM.py
@@ -4,47 +4,115 @@
 
 import pygmsh as pg
 import numpy as np
+from _hotshot import resolution
 
 
-def generate(resolution):
+def generate_3D_sensor_mesh(x=250., y=50., radius=6., nD=2, resolution=5.):
     geom = pg.Geometry()
 
-    # Two readout pillars
-    circle_left = geom.add_circle(x0=[-1.5, 0.0, 0.0],
-                                  radius=0.5,
-                                  lcar=resolution / 2.,
-                                  num_sections=4,
-                                  # If compound==False, the section borders have to be points of the
-                                  # discretization. If using a compound circle, they don't; gmsh can
-                                  # choose by itself where to point the circle points.
-                                  compound=False
-                                  )
-    circle_left_ll = geom.add_line_loop(circle_left)
+    pillars = []
 
-    circle_right = geom.add_circle(x0=[1.5, 0.0, 0.0],
-                                   radius=0.5,
-                                   lcar=resolution / 2.,
-                                   num_sections=4,
-                                   # If compound==False, the section borders have to be points of the
-                                   # discretization. If using a compound circle, they don't; gmsh can
-                                   # choose by itself where to point the circle points.
-                                   compound=False
-                                   )
-    circle_right_ll = geom.add_line_loop(circle_right)
+    # Create readout pillars
+    for pillar in range(nD):
+        position = x / nD * (pillar + 1. / 2.) - x/2.
+#         print position
+#         continue
+        circle = geom.add_circle(x0=[position, 0.0, 0.0],
+                                      radius=radius,
+                                      lcar=resolution / 2.,
+                                      num_sections=4,
+                                      # If compound==False, the section borders have to be points of the
+                                      # discretization. If using a compound circle, they don't; gmsh can
+                                      # choose by itself where to point the circle points.
+                                      compound=False
+                                      )
+        pillars.append(geom.add_line_loop(circle))
 
-    geom.add_rectangle(xmin=-3,
-                       xmax=3,
-                       ymin=-3,
-                       ymax=3,
-                       z=0,
-                       lcar=resolution,
-                       holes=[circle_left_ll, circle_right_ll])
+#     # Create bias pillars
+#     
+#     circle = geom.add_circle(x0=[-x/2. + radius, -y/2., 0.0],
+#                                   radius=radius,
+#                                   lcar=resolution / 2.,
+#                                   num_sections=2,
+#                                   # If compound==False, the section borders have to be points of the
+#                                   # discretization. If using a compound circle, they don't; gmsh can
+#                                   # choose by itself where to point the circle points.
+#                                   compound=False
+#                                   )
+#     print circle
+#     pillars.append(geom.add_line_loop(circle))
+    
+#     raise
+#     circle_right = geom.add_circle(x0=[1.5, 0.0, 0.0],
+#                                    radius=radius,
+#                                    lcar=resolution,
+#                                    num_sections=4,
+#                                    # If compound==False, the section borders have to be points of the
+#                                    # discretization. If using a compound circle, they don't; gmsh can
+#                                    # choose by itself where to point the circle points.
+#                                    compound=False
+#                                    )
+#     
+#     circle_right_ll = geom.add_line_loop(circle_right)
+    
+#     circle_top_left = geom.add_circle(x0=[1.5, 0.0, 0.0],
+#                                radius=0.5,
+#                                lcar=resolution,
+#                                num_sections=3,
+#                                # If compound==False, the section borders have to be points of the
+#                                # discretization. If using a compound circle, they don't; gmsh can
+#                                # choose by itself where to point the circle points.
+#                                compound=False)
+#            
+    print -y/2
+    circle_points = []
+    circle_points.append(geom.add_point([-x/4, -y/2, 0], lcar=resolution))
+    circle_points.append(geom.add_point([0, y, 0], lcar=resolution))
+    circle_points.append(geom.add_point([x/4, -y/2, 0], lcar=resolution))
+    circle_half = geom.add_circle_sector(circle_points)
+    
+    t = [circle_points[0] + circle_points[2]]
+    #circle_half_ll = geom.add_compound_line(circle_points)
+
+    points = []
+    
+    points_xyz = [
+        #[-x/4, -y/4, 0],
+        #[x/4, -y/4, 0],
+        [x/4, -y/2, 0],
+        [x/2, -y/2, 0],
+        [x/2, y/2, 0],
+        [-x/2, y/2, 0],
+        [-x/2, -y/2, 0],
+        [-x/4, -y/2, 0]
+        ]
+    for point in points_xyz:
+        points.append(geom.add_point(point, lcar=resolution))
+        
+    #points = circle_points + points
+    
+    # Create lines
+    lines = [geom.add_line(points[i], points[i+1]) for i in range(len(points)-1)]
+    #lines.append(geom.add_line(points[-1], points[0]))
+    
+    line_loop = geom.add_line_loop(lines)
+    geom.add_plane_surface([line_loop])
+    
+#     geom.add_polygon_loop(X, lcar=resolution)
+# 
+#     geom.add_rectangle(xmin=-x/2,
+#                        xmax=x/2,
+#                        ymin=-y/2,
+#                        ymax=y/2,
+#                        z=0,
+#                        lcar=resolution,
+#                        holes=pillars)
 
     return geom
 
 
 if __name__ == '__main__':
-    points, cells = pg.generate_mesh(generate(resolution=0.3))
+    points, cells = pg.generate_mesh(generate_3D_sensor_mesh())
     import matplotlib.pyplot as plt
 
     # Plot triangle cells
@@ -55,5 +123,5 @@ if __name__ == '__main__':
     plt.show()
 
     # Save mesh
-    # import meshio as mio
-    # mio.write('circle.vtk', points, cells)
+    import meshio as mio
+    mio.write('circle.msh', points, cells)
---
+++ siliconproperties/python_files/testFEM2.py
@@ -0,0 +1,64 @@
+import fipy
+
+import pygmsh as pg
+import meshio as mio
+
+
+def generate(resolution):
+    geom = pg.Geometry()
+
+    # Two readout pillars
+    circle = geom.add_circle(x0=[10., 0.0, 0.0],
+                                   radius=5,
+                                   lcar=resolution/2.,
+                                   num_sections=4,
+                                   # If compound==False, the section borders have to be points of the
+                                   # discretization. If using a compound circle, they don't; gmsh can
+                                   # choose by itself where to point the circle points.
+                                   compound=False
+                                   )
+    circle_ll = geom.add_line_loop(circle)
+
+    geom.add_rectangle(xmin=0,
+                       xmax=20,
+                       ymin=-10,
+                       ymax=10,
+                       z=0,
+                       lcar=resolution,
+                       holes=[circle_ll])
+
+    return geom
+
+
+if __name__ == '__main__':
+    # Save mesh
+    points, cells = pg.generate_mesh(generate(resolution=0.3))
+    mio.write('test0.msh', points, cells)
+#     import matplotlib.pyplot as plt
+#     plt.plot(points[cells['triangle'].T, 0], points[cells['triangle'].T, 1], '-', color='black')
+#     plt.plot(points[cells['line'].T, 0], points[cells['line'].T, 1], '-', color='black')
+#     plt.savefig('test.svg')
+    #plt.show()
+    raise
+    mesh = fipy.GmshImporter2D('test0.msh')
+    X,Y =  mesh.getFaceCenters()
+    
+    x0,y0 = 10, 0
+    r = 5
+    
+    potential = fipy.CellVariable(mesh=mesh, name='potential', value=0.)
+    permittivity = 1.
+    potential.equation = (fipy.DiffusionTerm(coeff = permittivity) == 0.)
+    
+    allfaces = mesh.getExteriorFaces()
+    ring = allfaces & ( (X-x0)**2+(Y-y0)**2 < (r+1.5)**2) 
+    
+    bcs = (
+        fipy.FixedValue(value=5.,faces=ring),
+        fipy.FixedValue(value=0.,faces=mesh.getFacesLeft()),
+    )
+    
+    potential.equation.solve(var=potential, boundaryConditions=bcs)
+    
+    viewer = fipy.viewers.Viewer(vars=(potential, ))
+    viewer.plotMesh("Example.png")
