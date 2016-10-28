f3b9cafc4c4ef15e62e6d18fb5ca732682c0b23b
DavidLP <David-Leon.Pohl@rub.de>
2016-10-28 21:52:04 +0200

    ENH: Different mesh sizes according to potential distribution

--- siliconproperties/python_files/testFEM.py
+++ siliconproperties/python_files/testFEM.py
@@ -69,7 +69,8 @@ def mesh_3D_sensor(x=250., y=50., radius=6., nD=2, resolution=10.):
 #     points = t + points
 
     # Create lines
-    lines = [geom.add_line(points[i], points[i + 1]) for i in range(len(points) - 1)]
+    lines = [geom.add_line(points[i], points[i + 1])
+             for i in range(len(points) - 1)]
     lines.append(geom.add_line(points[-1], points[0]))
 
     line_loop = geom.add_line_loop(lines)
@@ -78,20 +79,46 @@ def mesh_3D_sensor(x=250., y=50., radius=6., nD=2, resolution=10.):
     return geom
 
 
-def mesh_planar_sensor(x, thickness, resolution=3.):
+def mesh_planar_sensor(x, thickness, resolution=1.):
     geom = pg.Geometry()
+    resolution_x = x / resolution
 
-    geom.add_rectangle(xmin=-x / 2.,
-                       xmax=x / 2.,
-                       ymin=-thickness / 2.,
-                       ymax=thickness / 2.,
-                       z=0,
-                       lcar=resolution)
+    points_xyz = [
+        [x / 2, -thickness / 2, 0],
+        [x / 2, thickness / 2, 0],
+        [-x / 2, thickness / 2, 0],
+        [-x / 2, -thickness / 2, 0],
+    ]
 
+    points = []
+    points.append(geom.add_point(points_xyz[0], lcar=resolution_x))
+    points.append(geom.add_point(points_xyz[1], lcar=resolution_x))
+    points.append(geom.add_point(points_xyz[2], lcar=resolution_x))
+    points.append(geom.add_point(points_xyz[3], lcar=resolution_x))
+
+    # Create lines
+    lines = [geom.add_line(points[i], points[i + 1])
+             for i in range(len(points) - 1)]
+    lines.append(geom.add_line(points[-1], points[0]))
+
+    line_loop = geom.add_line_loop(lines)
+    geom.add_plane_surface([line_loop])
+
+    # Add 1/x1.5 law for the mesh size
+    raw_codes = ['lc = %f;' % (resolution_x / 4.),
+                 'Field[1] = Attractor;',
+                 'Field[1].EdgesList = {l2};'
+                 'Field[1].NNodesByEdge = %d;' % resolution,
+                 'Field[2] = MathEval;',
+                 'Field[2].F = Sprintf(\"F1^3 + %g\", lc);',
+                 'Background Field = 2;\n']
+
+    geom.add_raw_code(raw_codes)
     return geom
 
 
-def calculate_planar_sensor_potential(width, pitch, n_pixel, thickness, resolution, V_backplane, V_readout=0):
+def calculate_planar_sensor_potential(width, pitch, n_pixel, thickness,
+                                      resolution, V_backplane, V_readout=0):
     points, cells = pg.generate_mesh(mesh_planar_sensor(x=width * n_pixel,
                                                         thickness=thickness,
                                                         resolution=resolution))
@@ -110,14 +137,15 @@ def calculate_planar_sensor_potential(width, pitch, n_pixel, thickness, resoluti
     V_readout = V_readout
     readout_plane = mesh.getFacesTop()
 
-    #no_fill = width - pitch
     electrodes = readout_plane
     bcs = [fipy.FixedValue(value=V_backplane, faces=backplane)]
     X, _ = mesh.getFaceCenters()
     for pixel in range(n_pixel):
         pixel_position = width * (pixel + 1. / 2.) - width * n_pixel / 2.
         bcs.append(fipy.FixedValue(value=V_readout,
-                                   faces=electrodes & (X > pixel_position - pitch / 2.) & (X < pixel_position + pitch / 2.)))
+                                   faces=electrodes &
+                                   (X > pixel_position - pitch / 2.) &
+                                   (X < pixel_position + pitch / 2.)))
 
     potential.equation.solve(var=potential, boundaryConditions=bcs)
     return potential
@@ -134,30 +162,34 @@ if __name__ == '__main__':
     pitch = 5
     n_pixel = 3
     thickness = 50
-    resolution = 0.4
+    resolution = 200.
     V_backplane, V_readout = -1, 0
 
     potential = calculate_planar_sensor_potential(width, pitch, n_pixel, thickness, resolution, V_backplane, V_readout)
 
-    plot_mesh(potential.mesh)
-
-    print 'Interpolate'
-
+#     plot_mesh(potential.mesh)
+ 
     min_x, max_x = np.min(np.array(potential.mesh.getFaceCenters()[0])), np.max(np.array(potential.mesh.getFaceCenters()[0]))
     min_y, max_y = np.min(np.array(potential.mesh.getFaceCenters()[1])), np.max(np.array(potential.mesh.getFaceCenters()[1]))
-
-    print np.sum(np.square(potential.value))
-#     raise
+ 
     fit = interpolate_potential(potential, smoothing=1)
-
+ 
     # Plot potential
     xnew = np.linspace(min_x, max_x, 1000)
     ynew = np.linspace(min_y, max_y, 1000)
     phi = fit(xnew, ynew).T
     plt.contour(xnew, ynew, phi, 15, colors='black')
-    plt.pcolormesh(xnew, ynew, phi, cmap=cm.get_cmap('Blues'))
+    plt.pcolormesh(xnew, ynew, phi, cmap=cm.get_cmap('Blues'), vmin=V_backplane, vmax=V_readout)
     plt.colorbar()
-
+    
+#     print phi.shape
+#     plt.clf()
+#     plt.plot(ynew, phi.T[600])
+#     plt.plot(ynew, phi.T[500])
+#     plt.plot(ynew, phi.T[400])
+#     plt.show()
+#     raise
+ 
     # Plot E-Field
     xnew = np.linspace(min_x, max_x, 10)
     ynew = np.linspace(min_y, max_y, 10)
@@ -166,5 +198,6 @@ if __name__ == '__main__':
     E_y, E_x = np.gradient(z_new)
     E_x, E_y = -E_x, -E_y
     plt.streamplot(xnew, ynew, E_x, E_y, density=1.5, color='gray', arrowstyle='-')
-    plt.gca().set_aspect(1.)
     plt.show()
+    
+
