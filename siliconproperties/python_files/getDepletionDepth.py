1c0c81c5abcea133ef1a296e972a509774792185
DavidLP <David-Leon.Pohl@rub.de>
2016-10-29 14:24:18 +0200

    ENH: new p-type depletion depth plot

--- siliconproperties/python_files/getDepletionDepth.py
+++ siliconproperties/python_files/getDepletionDepth.py
@@ -22,10 +22,13 @@ def get_depletion_depth(V_bias, n_eff, temperature):
 
 if __name__ == '__main__':
     import matplotlib.pylab as plt
+    
+    from siliconproperties.python_files.getResistivity import get_resistivity
+
 
     V_bias = np.linspace(0, 100., 1000.)
-
-    # Plot diffusion potential
+ 
+    # Plot depletion depth for different bias voltages
     for n_eff in [1, 10, 100]:
         for temperature in [300]:
             depletion_depth = get_depletion_depth(
@@ -43,4 +46,37 @@ if __name__ == '__main__':
     plt.legend(loc=0)
     plt.grid()
     plt.savefig('DepletionDepth.pdf', layout='tight')
+    plt.show()
+    
+    # Plot depletion depth as a function of the resistivity
+    temperature = 300  # [K]
+    e_field = 1e3  # [V/cm]
+    V_bias = 100  # [V]
+    n_eff = np.logspace(0, 3, 1000.)
+    resistivity = get_resistivity(n_eff, 
+                                  is_n_type=False, 
+                                  temperature=temperature, 
+                                  e_field=e_field)
+    depletion_depth = get_depletion_depth(
+        V_bias=V_bias,
+        n_eff=n_eff,
+        temperature=temperature)
+    plt.plot(resistivity,
+             depletion_depth,
+             linewidth=2.,
+             label='$\mathrm{V_{bias}=%d\ V}$, $T=%d$'
+             % (V_bias, temperature))
+    
+    plt.plot(resistivity,
+             0.3 * np.sqrt(V_bias * resistivity),
+             '--',
+             linewidth=2.,
+             label=r'$0.3\sqrt{V_{bias}\ \mathrm{[V]}\cdot\rho\ \mathrm{[\Omega - cm]}}$')
+    
+    plt.title('Depletion depth in p-type silicon')
+    plt.xlabel('Resistivity [$\mathrm{\Omega - cm}$]')
+    plt.ylabel('Depletion depth [$\mathrm{um}$]')
+    plt.legend(loc=0)
+    plt.grid()
+    plt.savefig('DepletionDepthResistivity.pdf', layout='tight')
     plt.show()
