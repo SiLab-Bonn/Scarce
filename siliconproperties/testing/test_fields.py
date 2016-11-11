c1946be45faffefb4b39d4ce0b2062de6c21cc26
DavidLP <David-Leon.Pohl@rub.de>
2016-11-11 11:07:56 +0100

    ENH: new test cases

--- siliconproperties/testing/test_fields.py
+++ siliconproperties/testing/test_fields.py
@@ -4,6 +4,8 @@ import numpy as np
 from siliconproperties.python_files.getWeightingField import get_weighting_field
 from siliconproperties.python_files.getWeightingPotential import get_weighting_potential
 
+from siliconproperties.testing import tools
+
 
 class Test(unittest.TestCase):
 
@@ -14,27 +16,88 @@ class Test(unittest.TestCase):
 
     def tearDown(self):
         pass
-
-
-    def test_w_field_analytic(self):
-        ''' Check weighting potential/field of planar sensor.
-        
-        The check is if grad(-Phi) = E_w
+    
+    
+    def test_w_potential_analytic(self):
+        ''' Check analytic weighting potential calculation
+            for planar sensors.
         '''
 
+#         tools.create_fixture(get_weighting_potential,
+#                              x=np.linspace(-200, 200, 200), 
+#                              y=np.linspace(0, 250, 200), 
+#                              D=[100, 150, 200, 250], 
+#                              S=[5, 50, 100, 150, 250], 
+#                              is_planar=True)
+        
+        self.assertTrue(tools.check_with_fixture(get_weighting_potential,
+                             x=np.linspace(-200, 200, 200), 
+                             y=np.linspace(0, 250, 200), 
+                             D=[100, 150, 200, 250], 
+                             S=[5, 50, 100, 150, 250], 
+                             is_planar=True))
+        
+    def test_w_field_analytic(self):
+        ''' Check analytic weighting field calculation
+            for planar sensors.
+        '''
+        
+#         tools.create_fixture(get_weighting_field,
+#                              x=np.linspace(-200, 200, 200), 
+#                              y=np.linspace(0, 250, 200), 
+#                              D=[100, 150, 200, 250], 
+#                              S=[5, 50, 100, 150, 250], 
+#                              is_planar=True)
+
+        self.assertTrue(tools.check_with_fixture(get_weighting_field,
+                             x=np.linspace(-200, 200, 200), 
+                             y=np.linspace(0, 250, 200), 
+                             D=[100, 150, 200, 250], 
+                             S=[5, 50, 100, 150, 250], 
+                             is_planar=True))
+
+ 
+    def test_w_pot_field_analytic(self):
+        ''' Check weighting potential/field of planar sensor.
+         
+        The check is if grad(-Phi) = E_w
+        '''
+ 
         width = 100
         thickness = 200
         x = np.linspace(-width * 2, width * 2, 100)
         y = np.linspace(0, thickness, 100)
         xx, yy = np.meshgrid(x, y, sparse=True)
-
-        e_w = get_weighting_field(xx, yy, D=width, S=thickness, is_planar=True)
+ 
+        e_w_x, e_w_y = get_weighting_field(xx, yy, D=width, S=thickness, is_planar=True)
         p_w = get_weighting_potential(xx, yy, D=width, S=thickness, is_planar=True)
-        
-        self.assertTrue(np.allclose(e_w, np.gradient(-p_w)))
+         
+        e_w_y_2, e_w_x_2 = np.gradient(-p_w, np.gradient(y), np.gradient(x))
+         
+        print e_w_x[10]
+        print e_w_x_2[10]
+         
+        self.assertTrue(np.allclose(e_w_x, e_w_x_2))
         
 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+#     unittest.main()
+
+    width = 100
+    thickness = 200
+    x = np.linspace(-width * 2, width * 2, 1000)
+    y = np.linspace(0, thickness, 1000)
+    xx, yy = np.meshgrid(x, y, sparse=True)
+    
+    e_w_x, e_w_y = get_weighting_field(xx, yy, D=width, S=thickness, is_planar=True)
+    p_w = get_weighting_potential(xx, yy, D=width, S=thickness, is_planar=True)
+    
+    e_w_y_2, e_w_x_2 = np.gradient(-p_w, np.gradient(y), np.gradient(x))
+    
+    print e_w_x[10]
+    print e_w_x_2[10]
+    
+#     self.assertTrue(np.allclose(e_w_x, e_w_x_2))
+    
