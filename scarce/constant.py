8558a9c9c3c782e7a58a43549a74fa0f9fcb5358
DavidLP <David-Leon.Pohl@rub.de>
2016-11-12 21:14:59 +0100

    MAINT

--- scarce/constant.py
+++ scarce/constant.py
@@ -12,3 +12,6 @@ package_path = os.path.dirname(scarce.__file__)  # Get the absolute path of this
 TEST_FOLDER = os.path.join(package_path, 'testing')
 FIXTURE_FOLDER = os.path.join(package_path, 'testing/fixtures')
 DATA_FOLDER = os.path.join(package_path, 'scarce/scarce')
+
+with open(os.path.join(package_path, '../VERSION'), 'r') as f:
+    VERSION = scarce_version = f.readline().strip()
