472adc3b2f4b0266d295211c94d9257be5587bfd
davidlp <pohl@physik.uni-bonn.de>
2016-11-12 10:32:29 +0100

    ENH: add scarce program constants

--- scarce/constant.py
+++ scarce/constant.py
@@ -1,3 +1,13 @@
-# Constants
+import os
+
+import scarce
+
+# Silicon constants
 epsilon_s = 1.04e-10  # Permittivity of silicon [F/m]
 density = 2.3290  # Density of silicon [g cm^-3], wikipedia
+
+# Software constants
+# Get package path
+package_path = os.path.dirname(scarce.__file__)  # Get the absolute path of this software
+FIXTURE_FOLDER = os.path.join(package_path, 'testing/fixtures')
+DATA_FOLDER = os.path.join(package_path, 'scarce/scarce')
