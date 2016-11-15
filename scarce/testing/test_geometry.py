5f39a751ff770f66337d9e16f32fa3c7776d24b3
DavidLP <David-Leon.Pohl@rub.de>
2016-11-15 19:46:23 +0100

    ENH: new test case for geometry functions

---
+++ scarce/testing/test_geometry.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+import meshio
+
+from scarce import geometry
+from scarce import constant
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        os.remove('planar_mesh_tmp.msh')
+
+    def test_mesh_planar(self):
+        ''' Check the mesh generation for planar sensor.
+        Likely due to random pertubation (on purpose) the result
+        is never exactly constant.
+        '''
+        
+        n_pixel = 5
+        width = 50
+        thickness = 200
+        
+        geometry.mesh_planar_sensor(n_pixel, 
+                                    width, 
+                                    thickness=thickness, 
+                                    resolution=100,
+                                    filename='planar_mesh_tmp.msh')
+        
+        points, _, _, _, _ = meshio.read('planar_mesh_tmp.msh')
+        
+        self.assertGreater(points.shape[0], 14000)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
