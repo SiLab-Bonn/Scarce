cbe14ca36c2c9ca3d59663a4a6a34b50a2737aae
DavidLP <David-Leon.Pohl@rub.de>
2016-11-15 19:45:40 +0100

    ENH: planar plot can also plot mesh API: planar plotting

--- scarce/plot.py
+++ scarce/plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from fipy.tools import numerix
 import numpy as np
 from matplotlib.collections import PolyCollection
+from matplotlib.patches import Rectangle
 from matplotlib import colors, cm
 
 
@@ -23,9 +24,7 @@ def get_mesh_plot(fig, mesh, values=None, invert_y_axis=True):
         polys.append(zip(x, y))
 
     collection = PolyCollection(polys)
-    #collection.set_linewidth(0.3 * resolution)
-    collection.set_facecolors('white')
-    #fig.set_aspect(1.)
+    collection.set_facecolors('None')
     if invert_y_axis:
         ax.invert_yaxis()
     ax.add_collection(collection)
@@ -43,60 +42,91 @@ def get_mesh_plot(fig, mesh, values=None, invert_y_axis=True):
 def plot_mesh(mesh, values=None, invert_y_axis=True):
     fig = plt.figure()
     get_mesh_plot(fig, mesh, values=values, invert_y_axis=invert_y_axis)
+    fig.gca().set_aspect('equal')
     plt.show()
-
-def plot_planar_sensor(potential_function,
-                       width,
+    
+def plot_planar_sensor(width,
                        pitch,
+                       thickness,
                        n_pixel,
                        V_backplane, 
-                       V_readout, 
-                       min_x, max_x, 
-                       min_y, max_y,
-                       field_function=None):
+                       V_readout,
+                       potential_function=None,
+                       field_function=None,
+                       mesh=None):
+    fig = plt.figure()
+    get_planar_sensor_plot(fig, 
+                       width,
+                       pitch,
+                       thickness,
+                       n_pixel,
+                       V_backplane, 
+                       V_readout,
+                       potential_function=potential_function,
+                       field_function=field_function,
+                       mesh=mesh)
+    plt.show()
+
+def get_planar_sensor_plot(fig, 
+                       width,
+                       pitch,
+                       thickness,
+                       n_pixel,
+                       V_backplane, 
+                       V_readout,
+                       potential_function=None,
+                       field_function=None,
+                       mesh=None):
+
+    ax = fig.add_subplot(111)
+    
+    min_x, max_x=-width * float(n_pixel) / 2., width * float(n_pixel) / 2.
+    min_y, max_y = 0., thickness
 
     # Define plot space
     x = np.linspace(min_x, max_x, 1000)
     y = np.linspace(min_y, max_y, 1000)
 #     y = np.square(np.linspace(np.sqrt(min_y), np.sqrt(max_y), 1000)) TODO: make this work with streamplot
 
-    # Interpolate potential to a x,y grid
+    # Create x,y plot grid
     xx, yy = np.meshgrid(x, y, sparse=True)
-    phi = potential_function(xx, yy)
 
-    # Plot Potential
-    
-    # BUG in matplotlib: aspect to be set to equal, otherwise contour plot wrong aspect ratio
-    # http://stackoverflow.com/questions/28857673/wrong-aspect-ratio-for-contour-plot-with-python-matplotlib
-    plt.gca().set_aspect('equal')
-    plt.contour(x, y, phi, 10, colors='black')
-    plt.pcolormesh(x, y, phi, cmap=cm.get_cmap('Blues'), vmin=V_backplane, vmax=V_readout)
-    plt.colorbar()
+    if potential_function:
+        # Plot Potential
+        phi = potential_function(xx, yy)
+        # BUG in matplotlib: aspect to be set to equal, otherwise contour plot wrong aspect ratio
+        # http://stackoverflow.com/questions/28857673/wrong-aspect-ratio-for-contour-plot-with-python-matplotlib
+        ax.set_aspect('equal')
+        ax.contour(x, y, phi, 10, colors='black')
+        cmesh = ax.pcolormesh(x, y, phi, cmap=cm.get_cmap('Blues'), vmin=V_backplane, vmax=V_readout)
+        cax = fig.add_axes([ax.get_position().xmax, 0.1, 0.05, ax.get_position().ymax- ax.get_position().ymin])
+        fig.colorbar(cmesh, cax=cax, orientation='vertical')
     
     # Plot E-Field
     if field_function:
-        x = np.linspace(min_x, max_x, 20)
-        y = np.linspace(min_y, max_y, 20)
-        xx, yy = np.meshgrid(x, y, sparse=True)
         E_x, E_y = field_function(xx, yy)
-    else:
+        ax.streamplot(x, y, E_x, E_y, density=1.0, color='gray', arrowstyle='-')
+    elif potential_function:
         E_x, E_y  = np.gradient(-phi, np.gradient(x), np.gradient(y), axis=(1, 0))
-
-    plt.streamplot(x, y, E_x, E_y, density=1.0, color='gray', arrowstyle='-')
+        ax.streamplot(x, y, E_x, E_y, density=1.0, color='gray', arrowstyle='-')    
+        
+    if mesh:
+        get_mesh_plot(fig, mesh, invert_y_axis=False)
    
-    # Plot pixel background
-    plt.gca().add_patch(plt.Rectangle((min_x, plt.ylim()[1]), (max_x - min_x), 0.05*(plt.ylim()[1] - plt.ylim()[0]), color="grey", linewidth=0))
+    # Plot backside
+    ax.add_patch(Rectangle((min_x, max_y), (max_x-min_x), max_y, color="grey", linewidth=2))
     
     # Plot pixel(s)
     for pixel in range(n_pixel):
         pixel_position = width * (pixel + 1. / 2.) - width * n_pixel / 2.
-        plt.gca().add_patch(plt.Rectangle((pixel_position - pitch / 2, plt.ylim()[0]), pitch, 0.05*(plt.ylim()[1] - plt.ylim()[0]), color="darkred", linewidth=0))
+        ax.add_patch(Rectangle((pixel_position - pitch / 2, ax.get_ylim()[0]), pitch, 0, color="darkred", linewidth=5))
+        ax.plot([pixel_position - pitch / 2, pixel_position - pitch / 2], [min_y, max_y], '--', color='black', linewidth=4)
+    ax.plot([pixel_position + pitch / 2, pixel_position + pitch / 2], [min_y, max_y], '--', color='black', linewidth=4)  # Last pixel end
     
-    plt.ylim((1.05 * min_y, 1.05 * max_y))
-    plt.xlabel('Position x/y [um]', fontsize=22)
-    plt.ylabel('Position z [um]', fontsize=22)
-    plt.gca().invert_yaxis()
-    plt.show()
+    ax.set_ylim((- 0.02*(ax.get_ylim()[1] - ax.get_ylim()[0]), 1.02 * max_y))
+    ax.set_xlabel('Position x/y [um]', fontsize=22)
+    ax.set_ylabel('Position z [um]', fontsize=22)
+    ax.invert_yaxis()
 
 
 def plot_3D_sensor(potential_function, pitch_x, pitch_y, n_pixel, radius, V_readout, V_bias, 
@@ -150,24 +180,32 @@ if __name__ == '__main__':
     from scarce import fields
     
     thickness = 200  # [um]
-    width = 40  # [um]
+    width = 250  # [um]
 
     def potential_function(x, y):
-        return fields.get_weighting_potential(x, y, D=thickness, S=width, is_planar=True)
+        return fields.get_weighting_potential_analytic(x, y, D=thickness, S=width, is_planar=True)
     
     def field_function(x, y):
-        return fields.get_weighting_field(x, y, D=thickness, S=width, is_planar=True)
+        return fields.get_weighting_field_analytic(x, y, D=thickness, S=width, is_planar=True)
     
-    field_function = None
-    
-    plot_planar_sensor(potential_function=potential_function, 
-                       width=width, 
-                       pitch=width, 
-                       n_pixel=1, 
+    from scarce import geometry
+    width = 50
+    pitch = 50
+    n_pixel = 5
+    thickness = 200
+    resolution = 50
+    V_backplane, V_readout = -1, 0
+
+#     mesh=geometry.mesh_planar_sensor(x=width, 
+#                                      thickness=thickness, 
+#                                      resolution=resolution)
+
+    plot_planar_sensor(width=width, 
+                       pitch=width,
+                       thickness=thickness,
+                       n_pixel=n_pixel, 
                        V_backplane=0, 
                        V_readout=1, 
-                       min_x=-width * 2, 
-                       max_x=width * 2, 
-                       min_y=0, 
-                       max_y=thickness,
-                       field_function=field_function)
+                       potential_function=potential_function,
+                       field_function=field_function,
+                       mesh=None)
