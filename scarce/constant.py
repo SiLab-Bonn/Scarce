896a4630f7226b1666890378e8bae8eb35549d85
DavidLP <david-leon.pohl@rub.de>
2016-11-18 10:32:28 +0100

    ENH: failing version query should not break package

--- scarce/constant.py
+++ scarce/constant.py
@@ -13,5 +13,8 @@ TEST_FOLDER = os.path.join(package_path, 'testing')
 FIXTURE_FOLDER = os.path.join(package_path, 'testing/fixtures')
 DATA_FOLDER = os.path.join(package_path, 'scarce/scarce')
 
-with open(os.path.join(package_path, '../VERSION'), 'r') as f:
-    VERSION = scarce_version = f.readline().strip()
+try:
+    with open(os.path.join(package_path, '../VERSION'), 'r') as f:
+        VERSION = scarce_version = f.readline().strip()
+except IOError:
+    VERSION = 'UNKNOWN'
