800575e9e00ad9f4252b48197aff901e0d348343
DavidLP <david-leon.pohl@rub.de>
2016-11-28 19:31:18 +0100

    MAINT: formatting

--- scarce/testing/test_solver.py
+++ scarce/testing/test_solver.py
@@ -18,14 +18,14 @@ class TestSolver(unittest.TestCase):
         n_eff = 5e12  # Effective doping concentration
         rho = constants.elementary_charge * n_eff * (1e-4) ** 3  # Charge density in C / um3
         epsilon = constant.epsilon_s * 1e-6  # Permitticity of silicon in F/um
-    
+
         # External voltages in V
         V_read = -0
-    
+
         # Geometry
         dx = 0.01  # Grid spacing / resolution
         L = 200.  # Length of simulation domain / width of sensor in um
-        
+
         # Create mesh
         nx = L / dx  # Number of space points
         mesh = fipy.Grid1D(dx=np.ones((int(nx), )) * dx, nx=nx)
@@ -34,11 +34,11 @@ class TestSolver(unittest.TestCase):
         for V_bias in range(-10, -200, -20):
             # Get 1D potential with numerical solver
             potential = potential_1D.get_potential(mesh, rho, epsilon, L, V_read, V_bias)
-        
+
             # Get correct analytical solution
             potential_a = fields.get_potential_planar_analytic_1D(X, V_bias=V_bias, V_readout=V_read, n_eff=n_eff, D=L)
-            
-            self.assertTrue(np.allclose(potential, potential_a[:-1], atol = 1e-1))
+
+            self.assertTrue(np.allclose(potential, potential_a[:-1], atol=1e-1))
 
 if __name__ == "__main__":
     unittest.main()
