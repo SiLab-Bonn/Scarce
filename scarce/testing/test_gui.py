a42d880a5f8c71315a2a2dc5e4e3f495ca3dea7c
DavidLP <david-leon.pohl@rub.de>
2016-11-30 11:24:14 +0100

    ENH: new gui test case

---
+++ scarce/testing/test_gui.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import sys
+
+from PyQt5 import QtWidgets
+
+from scarce.gui import main
+
+
+class TestGui(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Linux CI travis runs headless, thus virtual x server is needed for gui testing
+        if os.getenv('TRAVIS', False):
+            from xvfbwrapper import Xvfb
+            cls.vdisplay = Xvfb()
+            cls.vdisplay.start()
+
+        # Create Gui
+        cls.app = QtWidgets.QApplication(sys.argv)
+        cls.gui = main.ApplicationWindow()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.gui.close()
+
+    def test_device_tab(self):
+        ''' Not implemented
+        '''
+
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
