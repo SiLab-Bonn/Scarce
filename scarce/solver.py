2d17ee730978cf47d949480f0587f2c8e83ab2bf
DavidLP <David-Leon.Pohl@rub.de>
2016-12-06 17:10:30 +0100

    MAINT

--- scarce/solver.py
+++ scarce/solver.py
@@ -1,6 +1,7 @@
 r"""The solver module provides numerical ODE solver functions.
 
-These functions are either implemented here or provide an interface to other solvers.
+These functions are either implemented here or provide an interface
+to other solvers.
 """
 
 import fipy
@@ -46,7 +47,9 @@ def solve(var, equation, **kwargs):
 
 class DriftDiffusionSolver(object):
 
-    ''' Solve the drift-diffusion equation for pseudo particles via euler forward difference + Monte Carlo diffusion
+    ''' Solve the drift-diffusion equation for pseudo particles.
+
+        Solving via euler forward difference.
     '''
 
     def __init__(self, pot_descr, pot_w_descr,
@@ -74,8 +77,8 @@ class DriftDiffusionSolver(object):
         self.T = T
 
     def solve(self, p0, q0, dt, n_steps=4000):
-        ''' Solve the drift diffusion equation for quasi partciles and calculates the
-            total induced current.
+        ''' Solve the drift diffusion equation for quasi partciles and calculates
+            the total induced current.
 
             Parameters
             ----------
@@ -89,48 +92,45 @@ class DriftDiffusionSolver(object):
                 Time step in ns. Influences presicion.
 
             n_steps : number
-                Number of steps in time. Has to be large enough that all particles
-                reach the readout electrode.
+                Number of steps in time. Has to be large enough that all
+                particles reach the readout electrode.
         '''
 
         # Start positions
         p_e, p_h = p0, p0.copy()
 
-#         dx = 0.1
-#         E_tot = 0
-#
-#         for x in np.arange(100., 0., -dx):
-#             E_tot += self.pot_descr.get_field(0., x)[1] * dx
+        # Result arrays initialized to NaN
+        traj_e = np.empty(shape=(n_steps, p_e.shape[0], p_e.shape[1]))
+        traj_h = np.empty(shape=(n_steps, p_h.shape[0], p_h.shape[1]))
+        I_ind_e = np.empty(shape=(n_steps, p_e.shape[1]))
+        I_ind_h = np.empty(shape=(n_steps, p_h.shape[1]))
+        traj_e[:], traj_h[:] = np.nan, np.nan
+        I_ind_e[:], I_ind_h[:] = np.nan, np.nan
 
-#         print 'E_tot', E_tot
-#         print np.sum([self.pot_descr.get_field(0., x) * dx for x ])
+        progress_bar = progressbar.ProgressBar(
+            widgets=['', progressbar.Percentage(), ' ',
+                     progressbar.Bar(marker='*', left='|', right='|'), ' ',
+                     progressbar.AdaptiveETA()],
+            maxval=n_steps, term_width=80)
 
-#         print p_e.shape
-        # Result arrays
-        traj_e = np.zeros(shape=(n_steps, p_e.shape[0], p_e.shape[1]))   # Electron trajectories
-        traj_h = np.zeros(shape=(n_steps, p_h.shape[0], p_h.shape[1]))   # Electron trajectories
-        I_ind_e = np.zeros(shape=(n_steps, p_e.shape[1]))   # Current induced from electrons
-        I_ind_h = np.zeros(shape=(n_steps, p_h.shape[1]))   # Current induced from holes
-
-        progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ',
-                                                        progressbar.Bar(marker='*', left='|', right='|'), ' ',
-                                                        progressbar.AdaptiveETA()],
-                                               maxval=n_steps, term_width=80)
         progress_bar.start()
 
         def in_boundary(description, x, y):
             ''' Checks if the particles are still in the bulk and should be propagated
             '''
 
-            sel_x = np.logical_and(x >= description.min_x, x <= description.max_x)
-            sel_y = np.logical_and(y >= description.min_y, y <= description.max_y)
+            sel_x = np.logical_and(x >= description.min_x,
+                                   x <= description.max_x)
+            sel_y = np.logical_and(y >= description.min_y,
+                                   y <= description.max_y)
             return np.logical_and(sel_x, sel_y)
 
         sel_e = np.ones(p_e.shape[1], dtype=np.bool)
         sel_h = np.ones(p_h.shape[1], dtype=np.bool)
 
         for step in range(n_steps):
-            if not np.any(sel_e) and not np.any(sel_h):  # All particles out of boundary
+            # Check if all particles out of boundary
+            if not np.any(sel_e) and not np.any(sel_h):
                 break
 
             # Electric field in V/cm
@@ -138,28 +138,32 @@ class DriftDiffusionSolver(object):
             E_h = self.pot_descr.get_field(p_h[0, sel_h], p_h[1, sel_h]) * 1e4
 
             # Mobility in cm2 / Vs
-            mu_e = silicon.get_mobility(np.sqrt(E_e[0] ** 2 + E_e[1] ** 2), temperature=self.T, is_electron=True)
-            mu_h = silicon.get_mobility(np.sqrt(E_h[0] ** 2 + E_h[1] ** 2), temperature=self.T, is_electron=False)
+            mu_e = silicon.get_mobility(np.sqrt(E_e[0] ** 2 + E_e[1] ** 2),
+                                        temperature=self.T, is_electron=True)
+            mu_h = silicon.get_mobility(np.sqrt(E_h[0] ** 2 + E_h[1] ** 2),
+                                        temperature=self.T, is_electron=False)
 
             # Velocity in cm / s
             v_e, v_h = - E_e * mu_e, E_h * mu_h
 
             # Calculate induced current
+            if np.any(p_e[0, sel_e]):  # Only if electrons are still drifting
+                # Weighting field in V/um
+                W_e = self.pot_w_descr.get_field(p_e[0, sel_e],
+                                                 p_e[1, sel_e])
+                # Induced current in C/s
+                I_ind_e[step, sel_e] = (W_e[0, 0] * v_e[0, 0] +
+                                        W_e[1, 0] * v_e[1, 0]) \
+                    - q0[sel_e] * dt * 1e-5
 
-            # Weighting field in V/cm
-            print 'p_e[0, sel_e]', p_e[0, sel_e]
-
-            W_e = self.pot_w_descr.get_field(p_e[0, sel_e], p_e[1, sel_e]) * 1e4
-            W_h = self.pot_w_descr.get_field(p_h[0, sel_h], p_h[1, sel_h]) * 1e4
-
-            print 'W_e.shape', W_e.shape
-            print 'v_e.shape', v_e.shape
-
-            if np.any(p_e[0, sel_e]):
-                I_ind_e[step, sel_e] = (W_e[0, 0] * v_e[0, 0] + W_e[1, 0] * v_e[1, 0]) * q0[sel_e] * dt
-
-            if np.any(p_h[0, sel_h]):
-                I_ind_h[step, sel_h] = (W_h[0, 0] * v_h[0, 0] + W_h[1, 0] * v_h[1, 0]) * q0[sel_h] * dt
+            if np.any(p_h[0, sel_h]):  # Only if holes are still drifting
+                # Weighting field in V/um
+                W_h = self.pot_w_descr.get_field(p_h[0, sel_h],
+                                                 p_h[1, sel_h])
+                # Induced current in C/s
+                I_ind_h[step, sel_h] = (W_h[0, 0] * v_h[0, 0] +
+                                        W_h[1, 0] * v_h[1, 0]) * \
+                    q0[sel_h] * dt * 1e-5
 
             # Position change in um
             d_p_e, d_p_h = v_e * dt * 1e-5, v_h * dt * 1e-5
@@ -169,8 +173,10 @@ class DriftDiffusionSolver(object):
             p_h[:, sel_h] = p_h[:, sel_h] + d_p_h
 
             # Check boundaries and update selection
-            sel_e = in_boundary(description=self.pot_descr, x=p_e[0, :], y=p_e[1, :])
-            sel_h = in_boundary(description=self.pot_descr, x=p_h[0, :], y=p_h[1, :])
+            sel_e = in_boundary(description=self.pot_descr,
+                                x=p_e[0, :], y=p_e[1, :])
+            sel_h = in_boundary(description=self.pot_descr,
+                                x=p_h[0, :], y=p_h[1, :])
             p_e[:, ~sel_e] = np.nan
             p_h[:, ~sel_h] = np.nan
 
