405bd267f3c8cce7193a12bf1e13d3f411e273e7
DavidLP <David-Leon.Pohl@rub.de>
2016-12-19 07:54:52 +0100

    ENH: smaller mesh at electrodes MAINT: formatting

--- scarce/geometry.py
+++ scarce/geometry.py
@@ -2,11 +2,12 @@
 
 import numpy as np
 import logging
-_LOGGER = logging.getLogger(__name__)
 
 from fipy import GmshImporter2D
 import pygmsh as pg
 import meshio as mio
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class SensorDescription3D(object):
@@ -15,10 +16,13 @@ class SensorDescription3D(object):
 
     """
 
-    def __init__(self, width_x, width_y, n_pixel_x, n_pixel_y, radius, nD, x0=0., y0=0.):
+    def __init__(self, width_x, width_y, n_pixel_x, n_pixel_y,
+                 radius, nD, x0=0., y0=0.):
         if n_pixel_x < 1 or n_pixel_y < 1:
             raise RuntimeError(
-                'Invalid parameter n_pixel_x, n_pixel_y = %d, %d' % (n_pixel_x, n_pixel_y))
+                'Invalid parameter n_pixel_x, n_pixel_y = %d, %d' % (n_pixel_x,
+                                                                     n_pixel_y)
+            )
 
         self.width_x = width_x
         self.width_y = width_y
@@ -56,13 +60,15 @@ class SensorDescription3D(object):
     def get_pixel_x_offsets(self):
         ''' Pixel center offsets in x for the number of pixels in x '''
 
-        for i in range(-self.get_n_pixel_left(), self.get_n_pixel_right() + 1, 1):
+        for i in range(-self.get_n_pixel_left(),
+                       self.get_n_pixel_right() + 1, 1):
             yield i * self.width_x
 
     def get_pixel_y_offsets(self):
         ''' Pixel center offsets in y for the number of pixels in y '''
 
-        for i in range(-self.get_n_pixel_top(), self.get_n_pixel_bottom() + 1, 1):
+        for i in range(-self.get_n_pixel_top(),
+                       self.get_n_pixel_bottom() + 1, 1):
             yield i * self.width_y
 
     def get_side_col_x_offsets(self):
@@ -72,7 +78,8 @@ class SensorDescription3D(object):
         for ix0 in self.get_pixel_x_offsets():
             for pillar in range(self.nD):
                 offsets.append(
-                    self.width_x / self.nD * (pillar + 1.) - self.width_x / 2. + ix0)
+                    self.width_x / self.nD * (pillar + 1.) -
+                    self.width_x / 2. + ix0)
         return offsets[:-1]  # Last offset is edge column
 
     def get_side_col_y_offsets(self):
@@ -155,12 +162,12 @@ class SensorDescription3D(object):
 
     def position_in_center_pixel(self, x, y):
         return -self.width_x / 2. <= x + self.x0 <= self.width_x / 2. \
-           and -self.width_y / 2. <= y + self.y0 <= self.width_y / 2.
+            and -self.width_y / 2. <= y + self.y0 <= self.width_y / 2.
 
     def position_in_column(self, x, y, incl_sides=False):
         ''' Returns mask wich is true if x and y is within a column radius.
             All columns that are full within the bulk (no edge, corner columns)
-            are used.
+            are used. Ifincl_sides is true also edge and side columns are used.
         '''
 
         # To make this function work with single numbers instead of arrays
@@ -205,14 +212,11 @@ def mesh_3D_sensor(width_x, width_y,
         pillars = []
 
         for position_x, position_y in desc.get_ro_col_offsets():
-            circle = geom.add_circle(x0=[position_x + x0, position_y + y0, 0.0],
+            circle = geom.add_circle(x0=[position_x + x0,
+                                         position_y + y0, 0.0],
                                      radius=radius,
                                      lcar=resolution,
                                      num_sections=4,
-                                     # If compound==False, the section borders have to be points of the
-                                     # discretization. If using a compound circle, they don't; gmsh can
-                                     # choose by itself where to point the
-                                     # circle points.
                                      compound=False
                                      )
             # Check if circle belongs to center pixel
@@ -227,14 +231,12 @@ def mesh_3D_sensor(width_x, width_y,
         pillars = []
 
         for position_x, position_y in desc.get_center_bias_col_offsets():
-            circle = geom.add_circle(x0=[position_x + x0, position_y + y0, 0.0],
+            circle = geom.add_circle(x0=[position_x + x0,
+                                         position_y + y0,
+                                         0.0],
                                      radius=radius,
                                      lcar=resolution,
                                      num_sections=4,
-                                     # If compound==False, the section borders have to be points of the
-                                     # discretization. If using a compound circle, they don't; gmsh can
-                                     # choose by itself where to point the
-                                     # circle points.
                                      compound=False
                                      )
 
@@ -255,8 +257,10 @@ def mesh_3D_sensor(width_x, width_y,
         loop = []
 
         def add_partial_column(x0, y0, last=False):
-            circle = geom.add_circle_sector(
-                [points[-1] if last else points[-2], geom.add_point([x0, y0, 0], lcar=resolution_x), points[0] if last else points[-1]])
+            center = geom.add_point([x0, y0, 0], lcar=resolution_x)
+            start = points[-1] if last else points[-2]
+            stop = points[0] if last else points[-1]
+            circle = geom.add_circle_sector([start, center, stop])
 
             # Check if circle belongs to center pixel
             if desc.position_in_center_pixel(x0, y0):
@@ -335,7 +339,7 @@ def mesh_3D_sensor(width_x, width_y,
 
         return [geom.add_line_loop(loop)]
 
-    def generate_3D_array(geom, width_x, width_y, r, resolution, x0=0., y0=0.):
+    def generate_3D_array(geom, r, resolution, x0=0., y0=0.):
         min_x, max_x, min_y, max_y = desc.get_array_corners()
 
         # Perimeter of 3D array, including edge and side columns
@@ -357,7 +361,7 @@ def mesh_3D_sensor(width_x, width_y,
 
         geom.add_plane_surface(perimeter + ro_pillars + bias_pillars)
 
-        raw_codes = ['lc = %f;' % (resolution_x / 8.),
+        raw_codes = ['lc = %f;' % (resolution_x / 40.),
                      'Field[1] = Attractor;',
                      'Field[1].EdgesList = {%s};' % ','.join(
                          high_res_segments),
@@ -370,22 +374,22 @@ def mesh_3D_sensor(width_x, width_y,
     geom = pg.Geometry()
     resolution_x = width_x / resolution
 
-    generate_3D_array(
-        geom, width_x, width_y, radius, resolution, x0=0., y0=0.)
+    generate_3D_array(geom, radius, resolution, x0=0., y0=0.)
 
     points, cells = pg.generate_mesh(geom, verbose=False)
     _LOGGER.info('Created mesh with %d points', len(points))
-
     mio.write(filename, points, cells)
-    return GmshImporter2D(filename)
+    return GmshImporter2D(filename), geom.get_code()
 
 
 # TODO: size independent resolution parameter
-def mesh_planar_sensor(n_pixel, width, thickness, resolution=1., filename='sensor.msh'):
+def mesh_planar_sensor(n_pixel, width, thickness,
+                       resolution=1., filename='sensor.msh'):
     _LOGGER.info('Mesh planar sensor array')
     if n_pixel < 3:
         raise logging.warning(
-            'Less than 3 pixels result in quite wrong boundaries. It is better to choose more pixelss!')
+            'Less than 3 pixels result in quite wrong boundaries. \
+            It is better to choose more pixelss!')
     if not n_pixel % 2:
         raise NotImplementedError(
             'Chose an odd pixel number (symmetry reasons)')
@@ -423,12 +427,12 @@ def mesh_planar_sensor(n_pixel, width, thickness, resolution=1., filename='senso
     geom.add_plane_surface([line_loop])
 
     # Add 1/x**4 law for the mesh size
-    raw_codes = ['lc = %f;' % (resolution_x / 6.),
+    raw_codes = ['lc = %f;' % (resolution_x / 40.),
                  'Field[1] = Attractor;',
                  'Field[1].EdgesList = {l3};'
                  'Field[1].NNodesByEdge = %d;' % resolution,
                  'Field[2] = MathEval;',
-                 'Field[2].F = Sprintf(\"F1^4 + %g\", lc);',
+                 'Field[2].F = Sprintf(\"F1^2 + %g\", lc);',
                  'Background Field = 2;\n']
 
     geom.add_raw_code(raw_codes)
