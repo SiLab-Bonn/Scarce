0c5932ca4483d727efc8cb2174b26264688d4b78
DavidLP <David-Leon.Pohl@rub.de>
2017-02-28 07:55:42 +0100

    ENH: finalize

--- scarce/examples/cce_planar.py
+++ scarce/examples/cce_planar.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import cm
 from scipy import integrate
-from scarce import plot, solver, geometry, silicon, fields, tools
+from scarce import plot, solver, geometry, silicon, fields, tools, analysis
 
 import time
 from matplotlib.pyplot import twinx
@@ -25,26 +25,122 @@ def timing(f):
     return wrap
 
 
-def transient_planar():
-    # Sensor parameters
-    n_eff = 1.7e12
-    width = 50.
-    thickness = 200.
-    temperature = 300.
-    pitch = 45.
+def cc_efficiency(fluences, biases, temperatures):
+    # Sensor bulk parameters
+    n_eff_0 = 1.475e12
+    is_ntype = True
+    is_oxygenated = True
+
     n_pixel = 9
-    V_bias = -80.
-    V_readout = 0.
-    resolution = 200
+    width = 50.
+    pitch = 30.
+    thickness = 250.
+
     smoothing = 0.05
+    resolution = 287
+
+    # Prepend no irradiation if not defined
+    if fluences[0] != 0.:
+        fluences = [0.] + fluences
+
+    try:
+        biases = float(biases)
+        biases = [biases] * len(fluences)
+    except TypeError:
+        pass
+
+    try:
+        temperatures = float(temperatures)
+        temperatures = [temperatures] * len(fluences)
+    except TypeError:
+        pass
+
+    # Charge collection efficiency maps
+    ccs = []
 
     # Create mesh of the sensor and stores the result
-    # The created file can be viewed with any mesh viewer (e.g. gmsh)
     mesh = geometry.mesh_planar_sensor(
         n_pixel=n_pixel,
         width=width,
         thickness=thickness,
         resolution=resolution)
+    # Numerically solve the Poisson equation on the mesh
+    w_potential = fields.calculate_planar_sensor_w_potential(
+        mesh=mesh,
+        width=width,
+        pitch=pitch,
+        n_pixel=n_pixel,
+        thickness=thickness)
+    min_x = float(mesh.getFaceCenters()[0, :].min())
+    max_x = float(mesh.getFaceCenters()[0, :].max())
+    nx = width * n_pixel
+    ny = thickness
+    pot_w_descr = fields.Description(w_potential,
+                                     min_x=min_x,
+                                     max_x=max_x,
+                                     min_y=0,
+                                     max_y=thickness,
+                                     nx=nx,
+                                     ny=ny,
+                                     smoothing=smoothing)
+
+    for i, fluence in enumerate(fluences):
+        n_eff = silicon.get_eff_acceptor_concentration(
+            fluence, n_eff_0 / 1e12, is_ntype, is_oxygenated) * 1e12
+        t_e_trapping = silicon.get_trapping(fluence * 1e12, is_electron=True,
+                                            paper=0)
+        t_h_trapping = silicon.get_trapping(fluence * 1e12, is_electron=False,
+                                            paper=0)
+        print 'n_eff', n_eff
+        print 't_r', t_e_trapping, t_h_trapping
+        edge_x, edge_y, charge = charge_collected(n_eff=n_eff, t_e_trapping=t_e_trapping, t_h_trapping=t_h_trapping,
+                                                  width=width, pitch=pitch, thickness=thickness, n_pixel=n_pixel,
+                                                  temperature=temperatures[i], V_bias=biases[i],
+                                                  pot_w_descr=pot_w_descr, mesh=mesh, smoothing=smoothing)
+        # Plot collected charge map
+        plt.clf()
+        plt.gca().set_aspect('equal')
+        plt.gca().invert_yaxis()
+        cmap = cm.get_cmap('inferno')
+        cmap.set_bad('white')
+        cmesh = plt.pcolormesh(edge_x, edge_y, charge,
+                               cmap=cmap, vmin=0, vmax=1.05)
+        plt.grid()
+        cax = plt.gcf().add_axes([plt.gca().get_position().xmax, 0.1, 0.05,
+                                  plt.gca().get_position().ymax - plt.gca().get_position().ymin])
+        plt.colorbar(cmesh, cax=cax, orientation='vertical')
+        plt.grid()
+        plt.title('Charge collection, fluence %1.2f neq_cm2' % fluence)
+        ccs.append(charge)
+        plt.savefig('CC_%d_%d.pdf' % (fluence, biases[i]), layout='tight')
+        #plt.show()
+
+        if i > 0:
+            # Plot charge collection efficiency map
+            plt.clf()
+            plt.gca().set_aspect('equal')
+            plt.gca().invert_yaxis()
+            cmap = cm.get_cmap('inferno')
+            cmap.set_bad('white')
+            cmesh = plt.pcolormesh(edge_x, edge_y, charge / ccs[0] * 100.,
+                                   cmap=cmap, vmin=0, vmax=100.)
+            plt.grid()
+            cax = plt.gcf().add_axes([plt.gca().get_position().xmax, 0.1, 0.05,
+                                      plt.gca().get_position().ymax - plt.gca().get_position().ymin])
+            plt.colorbar(cmesh, cax=cax, orientation='vertical')
+            plt.grid()
+            plt.title('Charge collection efficiency, '
+                      'fluence %1.2f neq_cm2' % fluence)
+            print 'CCE', fluence, charge.mean() / ccs[0].mean() * 100.
+            plt.savefig('CCE_%d_%d.pdf' % (fluence, biases[i]), layout='tight')
+            #plt.show()
+
+    return ccs
+
+
+def charge_collected(n_eff, t_e_trapping, t_h_trapping, width, pitch, thickness, n_pixel, temperature,
+                     V_bias, pot_w_descr, mesh, smoothing):
+    V_readout = 0.
 
     min_x = float(mesh.getFaceCenters()[0, :].min())
     max_x = float(mesh.getFaceCenters()[0, :].max())
@@ -73,196 +169,38 @@ def transient_planar():
                                    nx=nx,
                                    ny=ny,
                                    smoothing=smoothing)
-    # Numerically solve the Poisson equation on the mesh
-    w_potential = fields.calculate_planar_sensor_w_potential(
-        mesh=mesh,
-        width=width,
-        pitch=pitch,
-        n_pixel=n_pixel,
-        thickness=thickness)
-    pot_w_descr = fields.Description(w_potential,
-                                     min_x=min_x,
-                                     max_x=max_x,
-                                     min_y=0,
-                                     max_y=thickness,
-                                     nx=nx,
-                                     ny=ny,
-                                     smoothing=smoothing)
 
-    # Start parameters of e-h pairs
-    # Create n_pairs e-h pairs
-    n_pairs = 1
-    grid_x = 5  # grid spacing in x in um
-    grid_y = 5  # grid spacing in y in um
-    x_bins = int(width / grid_x)
-    y_bins = int(thickness / grid_y)
+    return analysis.get_charge_planar(width, thickness, pot_descr, pot_w_descr,
+                                      t_e_trapping=t_e_trapping, t_h_trapping=t_h_trapping,
+                                      grid_x=5, grid_y=5, n_pairs=10, dt=0.001,
+                                      n_steps=25000, temperature=300)
 
-    print x_bins, y_bins
-
-    range_x = (-width / 2., width / 2.)
-    range_y = (0, thickness)
-
-    # Create e-h pairs in the pixel, avoid charge carriers on boundaries
-    # e.g. x = -width / 2 or y = 0
-    xx, yy = np.meshgrid(np.linspace(range_x[0] + grid_x / 2.,
-                                     range_x[1] - grid_x / 2., x_bins),
-                         np.repeat(np.linspace(range_y[0] + grid_y / 2.,
-                                               range_y[1] - grid_y / 2., y_bins),
-                                   n_pairs),  # 10 e-h per position
-                         sparse=False)  # All combinations of x / y
-
-#     xx, yy = np.meshgrid(np.linspace(17.5,
-#                                      17.5, 1),
-#                          np.repeat(np.linspace(137.5,
-#                                                137.5, 1),
-#                                    n_pairs),  # 10 e-h per position
-#                          sparse=False)  # All combinations of x / y
-
-    p0 = np.array([xx.ravel(), yy.ravel()])  # Position [um]
-
-    # Initial charge set to 1
-    q_start = 1.
-    q0 = np.ones(p0.shape[1]) * q_start
-    q_max = q_start * 1.05
-
-    # Time steps
-    dt = 0.0001  # [ns]
-    n_steps = 250000
-
-    t = np.linspace(0, n_steps * dt, 1000)
-
-    dd = solver.DriftDiffusionSolver(pot_descr, pot_w_descr,
-                                     T=temperature, diffusion=True, save_frac=10)
-    traj_e, traj_h, I_ind_e, I_ind_h, T, _, Q_ind_e_tot, Q_ind_h_tot = dd.solve(p0, q0, dt, n_steps,
-                                                                                multicore=True)
-
-    # Trajectory at t=0 is start position
-    pos_0 = traj_e[0]
-
-    # Interpolate data to fixed time points for easier plotting
-#     I_ind_e = tools.time_data_interpolate(T, I_ind_e, t, axis=0, fill_value=0.)
-#     I_ind_h = tools.time_data_interpolate(T, I_ind_h, t, axis=0, fill_value=0.)
-    I_ind_e[np.isnan(I_ind_e)] = 0.
-    I_ind_h[np.isnan(I_ind_h)] = 0.
-    Q_ind_e = integrate.cumtrapz(I_ind_e, T, axis=0, initial=0)
-    Q_ind_h = integrate.cumtrapz(I_ind_h, T, axis=0, initial=0)
-
-    # Last index with data (time != nan)
-#     index = np.nanargmax(T, axis=0)
-#     y = np.indices(index.shape)
-    # Last recorded integrated charge is total induced charge
-#     q_ind = Q_ind_e[index, y][0] + Q_ind_h[index, y][0]
-    q_ind = Q_ind_e_tot + Q_ind_h_tot
-
-    # Histogram charge per start position
-    data = np.vstack((pos_0[0], pos_0[1], q_ind)).T
-
-    n_bins_c = 200
-    H, edges = np.histogramdd(sample=data,
-                              bins=(x_bins, y_bins, n_bins_c),
-                              range=((range_x[0], range_x[1]),
-                                     (range_y[0], range_y[1]),
-                                     (0., q_max)))
-
-#     sel = q_ind > 1.03
-    # Induced charge of one e-h pair
-#     plt.plot(T, Q_ind_e, '-',
-#              color='blue', linewidth=2, label='e')
-#     plt.plot(T, Q_ind_h, '-',
-#              color='red', linewidth=2, label='h')
-#     plt.plot(T, Q_ind_e + Q_ind_h,
-#              '-', color='magenta', linewidth=2, label='e+h')
-#     plt.legend(loc=0)
-#     plt.xlabel('Time [ns]')
-#     plt.ylabel('Total induced charge [a.u.]')
-
-#     plt.twinx()
-#     plt.plot(t[:, sel], Q_ind_e[:, sel], '.-',
-#              color='blue', linewidth=2, label='e')
-#     plt.plot(t[:, sel], Q_ind_h[:, sel], '.-',
-#              color='red', linewidth=2, label='h')
-#     plt.plot(t[:, sel], Q_ind_e[:, sel] + Q_ind_h[:, sel],
-#              '.-', color='magenta', linewidth=2, label='e+h')
-
-#     plt.show()
-#     print pos_0[0][sel], pos_0[1][sel]
-
-#     print range_x[0], range_x[1]
-#     print range_y[0], range_y[1]
-
-#     print (np.nansum(H, axis=2) == 0).shape
-#     print q_ind.shape
-#     raise
-
-#     charge_pos = np.ma.masked_array(data=np.zeros(shape=(x_bins, y_bins)),mask=np.zeros(shape=(x_bins, y_bins)))
-    charge_pos = np.zeros(shape=(x_bins, y_bins))
-    sel = (np.sum(H, axis=2) != 0)
-
-    weights = (edges[2][:-1] + edges[2][1:]) / 2.
-
-    charge_pos[sel] = np.average(
-        H, axis=2, weights=weights)[sel] * weights.sum() / np.sum(H, axis=2)[sel]
-
-    edges_x = (edges[0][:-1] + edges[0][1:]) / 2.
-    edges_y = (edges[1][:-1] + edges[1][1:]) / 2.
-
-    for xi, yi in zip(*np.where(charge_pos < 0.9)):
-        print edges_x[xi], edges_y[yi], charge_pos[xi, yi]
-        plt.clf()
-        plt.bar(weights, H[xi, yi], width=np.diff(weights)[0])
-        plt.show()
-        plt.clf()
-        sel = np.logical_and(pos_0[0] == edges_x[xi],
-                             pos_0[1] == edges_y[yi])
-        plt.plot(T[:, sel], Q_ind_e[:, sel] + Q_ind_h[:, sel])
-        for c in weights[H[xi, yi].astype(np.bool)]:
-            plt.plot(plt.xlim(), [c, c])
-        plt.twinx()
-        print traj_e.shape
-        plt.plot(T[:, sel], traj_e[:, 0, sel])
-        plt.plot(T[:, sel], traj_e[:, 1, sel])
-        plt.show()
-
-    plt.clf()
-    plt.gca().set_aspect('equal')
-    plt.gca().invert_yaxis()
-    cmap = cm.get_cmap('inferno')
-    cmap.set_bad('white')
-    cmesh = plt.pcolormesh(edges[0], edges[1], charge_pos.T,
-                           cmap=cmap, vmin=0, vmax=q_max)
-    plt.grid()
-    cax = plt.gcf().add_axes([plt.gca().get_position().xmax, 0.1, 0.05,
-                              plt.gca().get_position().ymax - plt.gca().get_position().ymin])
-    plt.colorbar(cmesh, cax=cax, orientation='vertical')
-    plt.grid()
-    plt.show()
-
-    # Plot numerical result in 2D with particle animation
-    fig = plt.figure()
-    plot.get_planar_sensor_plot(fig=fig,
-                                width=width,
-                                pitch=pitch,
-                                thickness=thickness,
-                                n_pixel=n_pixel,
-                                V_backplane=V_bias,
-                                V_readout=V_readout,
-                                pot_func=pot_descr.get_potential,
-                                field_func=pot_descr.get_field,
-                                depl_func=pot_descr.get_depletion,
-                                title='Planar sensor potential')
-
-    # Create animation
-    frames = 50
-    init, animate = plot.animate_drift_diffusion(fig, T=T, pe=traj_e,
-                                                 ph=traj_h, dt=t.max() /
-                                                 frames,
-                                                 n_steps=frames)
-    ani = animation.FuncAnimation(fig=fig, func=animate,
-                                  blit=True, init_func=init, frames=frames,
-                                  interval=5000 / frames)
-
-    # ani.save('Example_planar_drift.gif', dpi=80, writer='imagemagick')
-    plt.show()
+    #         # Plot numerical result in 2D with particle animation
+    #         fig = plt.figure()
+    #         plot.get_planar_sensor_plot(fig=fig,
+    #                                     width=width,
+    #                                     pitch=pitch,
+    #                                     thickness=thickness,
+    #                                     n_pixel=n_pixel,
+    #                                     V_backplane=V_bias,
+    #                                     V_readout=V_readout,
+    #                                     pot_func=pot_descr.get_potential,
+    #                                     field_func=pot_descr.get_field,
+    #                                     depl_func=pot_descr.get_depletion,
+    #                                     title='Planar sensor potential')
+    #
+    #         # Create animation
+    #         frames = 50
+    #         init, animate = plot.animate_drift_diffusion(fig, T=T, pe=traj_e,
+    #                                                      ph=traj_h, dt=t.max() /
+    #                                                      frames,
+    #                                                      n_steps=frames)
+    #         ani = animation.FuncAnimation(fig=fig, func=animate,
+    #                                       blit=True, init_func=init, frames=frames,
+    #                                       interval=5000 / frames)
+    #
+    #         # ani.save('Example_planar_drift.gif', dpi=80, writer='imagemagick')
+    #         plt.show()
 
 
 if __name__ == '__main__':
@@ -271,4 +209,22 @@ if __name__ == '__main__':
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S")
-    transient_planar()
+
+    # fluences = [0., 100.] + range(200, 900, 200) + range(1000, 4900, 1000) + range(5000, 11000, 2500)
+    fluences = [0.] + [1000] * 8
+    biases = [-80.] + range(-100, -801, -100)
+  
+    ccs = cc_efficiency(fluences=fluences,
+                        biases=biases,
+                        temperatures=300)
+  
+    ccs_mean = [cc.mean() / ccs[0].mean() * 100. for cc in ccs]
+    #ccs_mean = [100.0, 95.819278274882421, 91.492897261571542, 84.44507498549774, 78.023854736701296, 72.517359275661676, 67.581338385657943, 49.639626527411856, 38.39041632424324, 30.536375588272836, 24.835251542831479, 17.418036145128809, 13.384192805371708]
+    print biases
+    print ccs_mean
+#     biases =  np.abs([-100, -200, -300, -400, -500, -600, -700, -800])
+#     ccs_mean =  [30.315111552256575, 41.117291542671161, 48.451824125239519, 54.58876529456731, 58.059736778438165, 59.735573362868422, 60.508863282827484, 61.292900450661357]
+    plt.clf()
+    plt.plot(biases, ccs_mean, '.-')
+    
+    plt.show()
