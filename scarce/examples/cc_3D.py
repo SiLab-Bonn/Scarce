b3003e7aba662c8633aabcad56cbb6b403e5916a
DavidLP <David-Leon.Pohl@rub.de>
2017-10-31 11:04:42 +0100

    MAINT: formatting

--- scarce/examples/cc_3D.py
+++ scarce/examples/cc_3D.py
@@ -11,24 +11,29 @@ from matplotlib import cm
 from scarce import plot, silicon, analysis, sensor
 
 
-def cc(bias, temperature, n_eff, t_e_trapping, t_h_trapping):
-    pot_w_descr, pot_descr, geom_descr = sensor.sensor_3D(n_eff=n_eff,
-                                                          V_bias=bias,
-                                                          V_readout=V_readout,
-                                                          temperature=temperature,
-                                                          n_pixel_x=n_pixel_x,
-                                                          n_pixel_y=n_pixel_y,
-                                                          width_x=width_x,
-                                                          width_y=width_y,
-                                                          radius=radius,
-                                                          nD=nD,
-                                                          resolution=resolution,
-                                                          smoothing=smoothing)
+def cc(n_eff, bias, V_readout, temperature,
+       t_e_trapping, t_h_trapping,
+       n_pixel_x, n_pixel_y, width_x, width_y,
+       radius, nD, resolution, smoothing):
+    pot_w_dcr, pot_dcr, geom_dcr = sensor.sensor_3D(n_eff=n_eff,
+                                                    V_bias=bias,
+                                                    V_readout=V_readout,
+                                                    temperature=temperature,
+                                                    n_pixel_x=n_pixel_x,
+                                                    n_pixel_y=n_pixel_y,
+                                                    width_x=width_x,
+                                                    width_y=width_y,
+                                                    radius=radius,
+                                                    nD=nD,
+                                                    resolution=resolution,
+                                                    smoothing=smoothing)
 
-    return analysis.get_charge_3D(geom_descr, pot_descr, pot_w_descr,
-                                  t_e_trapping=t_e_trapping, t_h_trapping=t_h_trapping,
-                                  grid_x=5, grid_y=5, n_pairs=10, dt=0.001,
-                                  n_steps=20000, temperature=temperature), pot_descr
+    return analysis.get_charge_3D(geom_dcr, pot_dcr, pot_w_dcr,
+                                  t_e_trapping=t_e_trapping,
+                                  t_h_trapping=t_h_trapping,
+                                  grid_x=2.5, grid_y=2.5, n_pairs=20, dt=0.001,
+                                  n_steps=20000,
+                                  temperature=temperature), pot_descr
 
 if __name__ == '__main__':
     import logging
@@ -50,24 +55,27 @@ if __name__ == '__main__':
     smoothing = 0.1
 
     fluence = 000.
-    bias = -75
+    bias = -20
     n_eff_0 = 4.6e11
     temperature = 300
 
     n_eff = silicon.get_eff_acceptor_concentration(fluence, n_eff_0 / 1e12,
                                                    is_ntype=False,
                                                    is_oxygenated=True)[0] * 1e12
-    t_e_trapping = silicon.get_trapping(fluence * 1e12, is_electron=True, paper=1)
-    t_h_trapping = silicon.get_trapping(fluence * 1e12, is_electron=False, paper=1)
+    t_e_trapping = silicon.get_trapping(fluence * 1e12, is_electron=True,
+                                        paper=1)
+    t_h_trapping = silicon.get_trapping(fluence * 1e12, is_electron=False,
+                                        paper=1)
 
-    t_e_trapping = 300000  # 4.8
-    t_h_trapping = 300000  # 4.8
+    t_e_trapping = None  # 4.8
+    t_h_trapping = None  # 4.8
 
-    (edge_x, edge_y, charge), pot_descr = cc(bias=bias,
-                                             temperature=temperature,
-                                             n_eff=n_eff,
-                                             t_e_trapping=t_e_trapping,
-                                             t_h_trapping=t_h_trapping)
+    (edge_x, edge_y, charge), pot_descr = cc(n_eff, bias, V_readout,
+                                             temperature,
+                                             t_e_trapping, t_h_trapping,
+                                             n_pixel_x, n_pixel_y,
+                                             width_x, width_y,
+                                             radius, nD, resolution, smoothing)
 
     # Plot numerical potential result in 2D
     plot.plot_3D_sensor(width_x, width_y,
@@ -92,8 +100,9 @@ if __name__ == '__main__':
                            cmap=cmap, vmin=0, vmax=1.05)
     plt.title('Charge collection, fluence %1.2f neq_cm2' % fluence)
     plt.grid()
-    cax = plt.gcf().add_axes([plt.gca().get_position().xmax, 0.1, 0.05,
-                              plt.gca().get_position().ymax - plt.gca().get_position().ymin])
+    cax = plt.gcf().add_axes(
+        [plt.gca().get_position().xmax, 0.1, 0.05,
+         plt.gca().get_position().ymax - plt.gca().get_position().ymin])
     plt.colorbar(cmesh, cax=cax, orientation='vertical')
     plt.grid()
     plt.savefig('CC_%d_%d.pdf' % (fluence, bias), layout='tight')
